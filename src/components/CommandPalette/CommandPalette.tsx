6d12133b9d35ac679c516c02107fda1e61bd2a97
Brandon Bridges <brandonrbridges@outlook.com>
2023-05-12 12:56:16 +0100

    ignore TS build errors

--- src/components/CommandPalette/CommandPalette.tsx
+++ src/components/CommandPalette/CommandPalette.tsx
@@ -54,7 +54,7 @@ const CommandPalette = () => {
             leaveTo='opacity-0 scale-95'
           >
             <Dialog.Panel className='mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all'>
-              <Combobox onChange={(person) => (window.location = person.url)}>
+              <Combobox>
                 <div className='relative'>
                   <MagnifyingGlassIcon
                     className='pointer-events-none absolute left-4 top-3.5 h-5 w-5 text-gray-400'
